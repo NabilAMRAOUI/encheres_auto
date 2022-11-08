8fa77df437262464640e7dc042db25bbd9ed6d50
nabil <amr.nabz@gmail.com>
2022-11-08 16:42:01 +0100

    COMMIT

--- PageAnnonce.php
+++ PageAnnonce.php
@@ -26,20 +26,23 @@ $annonces = $query->fetchAll(PDO::FETCH_ASSOC);
     <header>
         <?php require __DIR__."/classes/navBar.php" ?>
     </header>
+    <br>
+    <div class="annonce-li">
+        <ul>
+            <li>
+                <?php
+                foreach ($annonces as $key => $value){ ?>
+                <p><?=$value["prix-depart"];?></p>
+                <p><?=$value["date-fin"];?></p>
+                <p><?=$value["modele"];?></p>
+                <p><?=$value["marque"];?></p>
+            <?php } ?>
+            </li>
+        </ul>
+    </div>
+    
 
-    <ul>
-        <li>
-            <?php
-            foreach ($annonces as $key => $value){ ?>
-            <p><?=$value["prix-depart"];?></p>
-            <p><?=$value["date-fin"];?></p>
-            <p><?=$value["modele"];?></p>
-            <p><?=$value["marque"];?></p>
-           <?php } ?>
-        </li>
-    </ul>
-
-    <a href="ajouter-annonce-anchere.php">Ajouter une annonce</a>
+    <a href="ajouter-annonce-enchere.php">Ajouter une annonce</a>
 
 
     
